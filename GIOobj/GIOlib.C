b6f48098064b561ca639d819c8a28817ea35d31a
cookgb <cookgb@7b32da70-8e03-4603-8d8e-ba0aa2ae4e7d>
2000-07-26 20:02:43 +0000

    Fixed to compile on SP2

--- GIOobj/GIOlib.C
+++ GIOobj/GIOlib.C
@@ -9,6 +9,20 @@
 #include "GIOlib.h"
 #include "GIOtempl.c"
 
+#ifdef SPX
+#pragma define(GIOseries<  float,  float, 1,GIO_FLOAT_SERIES>)
+#pragma define(GIOseries<  float,  float, 2,GIO_FLOAT_SERIES>)
+#pragma define(GIOseries<  float,  float, 3,GIO_FLOAT_SERIES>)
+#pragma define(GIOcseries< float,  float, 1,GIO_FLOAT_CSERIES>)
+#pragma define(GIOcseries< float,  float, 2,GIO_FLOAT_CSERIES>)
+#pragma define(GIOcseries< float,  float, 3,GIO_FLOAT_CSERIES>)
+#pragma define(GIOseries< double, double, 1,GIO_DOUBLE_SERIES>)
+#pragma define(GIOseries< double, double, 2,GIO_DOUBLE_SERIES>)
+#pragma define(GIOseries< double, double, 3,GIO_DOUBLE_SERIES>)
+#pragma define(GIOcseries< double, double, 1,GIO_DOUBLE_CSERIES>)
+#pragma define(GIOcseries< double, double, 2,GIO_DOUBLE_CSERIES>)
+#pragma define(GIOcseries< double, double, 3,GIO_DOUBLE_CSERIES>)
+#else
 template class  GIOseries<  float,  float, 1,GIO_FLOAT_SERIES>;
 template class  GIOseries<  float,  float, 2,GIO_FLOAT_SERIES>;
 template class  GIOseries<  float,  float, 3,GIO_FLOAT_SERIES>;
@@ -21,3 +35,4 @@ template class  GIOseries< double, double, 3,GIO_DOUBLE_SERIES>;
 template class  GIOcseries< double, double, 1,GIO_DOUBLE_CSERIES>;
 template class  GIOcseries< double, double, 2,GIO_DOUBLE_CSERIES>;
 template class  GIOcseries< double, double, 3,GIO_DOUBLE_CSERIES>;
+#endif
